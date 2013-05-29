56b7c8e82817c0c4b7974ec6b2f520e885d78db1
liyuray <liyuray@gmail.com>
2013-05-29 09:58:57 +0800

    add files

---
+++ README.md
@@ -0,0 +1,7 @@
+# blogger2epub #
+
+## get blogger pages ##
+wget --content-disposition -P cache -A html,jpg,png,bmp -U "Mozilla/5.0 (Linux; U; Android 2.3.3; zh-tw; HTC_Pyramid Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1" --restrict-file-names=nocontrol -D greenhornfinancefootnote.blogspot.tw,greenhornfinancefootnote.blogspot.com,bp.blogspot.com -nd -r -l1 -p -E -H -k -K http://greenhornfinancefootnote.blogspot.tw/2009/05/blog-post_10.html
+
+## produce epub ##
+perl blogger2epub.pl
---
+++ blogger2epub.pl
@@ -0,0 +1,157 @@
+use strict;
+use warnings;
+use File::Copy;
+#use File::Slurp;
+use EBook::EPUB;
+use File::Path qw(make_path);
+use Encode qw(decode encode);
+use autodie;
+
+my %titleh;
+my @titles = qw(all);
+
+for my $title (@titles) {
+  preprocess($title);
+  genepub($title);
+}
+
+sub get_head {
+  my $title = shift;
+  my $head = << "AAAA";
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:xml="http://www.w3.org/XML/1998/namespace" xml:lang="zh" lang="zh">
+<head>
+	<title>$title</title>
+</head>
+<body>
+AAAA
+  return $head;
+}
+
+sub preprocess {
+  my $title = shift;
+
+  my $tail = "</body></html>";
+
+  make_path("$title/epub");
+
+  chdir("$title/raw");
+  my %jpgh;
+  my %b5jpgh;
+  my %inhjpgh;
+  my @b5jpgs = (
+                <*.jpg>,
+                <*.jpg.[0-9]>,
+                <*.bmp>,
+                <*.bmp.[0-9]>,
+                <*.png>,
+                <*.png.[0-9]>,
+                <*.gif>,
+                <*.gif.[0-9]>,
+               );
+  my @ucjpgs = map {decode('big5',$_)} @b5jpgs;
+  @b5jpgh{@ucjpgs} = @b5jpgs;
+
+  chdir("../..");
+  my $i=0;
+  for my $jpg (@ucjpgs){
+    my $njpg = $jpg;
+    my $inhjpg = $jpg;
+    $inhjpg =~ s/\%/%25/g;
+    $inhjpg =~ s/\&/&amp;/g;
+    $njpg =~ tr/a-zA-Z0-9.//cd;
+    $njpg =~ s/\.(jpg|bmp|png|gif)\.(\d)/__$2\.$1/i;
+    #print $njpg,$/;
+    if ($jpg ne $njpg) {
+        $jpgh{$jpg} = sprintf("%04d",$i++)."_".$njpg;
+        $inhjpgh{$inhjpg} = $jpgh{$jpg};
+    } else {
+        $jpgh{$jpg} = $jpg;
+        $inhjpgh{$inhjpg} = $jpgh{$jpg};
+    }
+  }
+#  die;
+#  $jpgh{"Global+Bond+Fund+Holding%25E7%259B%25A7.jpg"} = "93GlobalBondFundHoldingE79BA7.jpg";
+#  $jpgh{"Global+Bond+Fund+Holding%25E7%25BE%258E%25E5%259C%258B.jpg"} = "94GlobalBondFundHoldingE7BE8EE59C8B.jpg";
+#  $jpgh{"%25E7%25BE%258E%25E5%259C%258Bvs%25E7%259B%25A7%25E6%25A3%25AE%25E5%25A0%25A1%25E7%25B8%25BE%25E6%2595%2588.jpg"} = "53E7BE8EE59C8BvsE79BA7E6A3AEE5A0A1E7B8BEE69588.jpg";
+
+  my $pattern = '(' . join('|', map {quotemeta} grep {$inhjpgh{$_} ne $_} keys %inhjpgh). ')';
+  my $match   = qr/"$pattern"/;
+#  print $match;
+  chdir("$title");
+  #my $i;
+  copy("raw/".$b5jpgh{$_}, "epub/".$jpgh{$_}) for @ucjpgs;
+  chdir("..");
+
+  chdir("$title/raw");
+  my @files = <*.html>;
+
+  for my $file (@files) {
+    my $fnoo = $file;
+#    $fnoo =~ s/\.html$/-2.html/;
+    my $fno = "../epub/$fnoo";
+#    $fno =~ s/\/raw\//\/epub\//;
+    open my $fh, "<:utf8", $file or die "cannot open < $!";
+    open my $fho, ">:utf8", $fno or die "cannot open > $!";
+    while (<$fh>) {
+      $titleh{$fnoo} = $1 if /<title>(.*)<\/title>/;
+      last if /<h3 class='post-title entry-title'>/;
+    }
+    print $fho get_head($titleh{$fnoo});
+    print $fho "<h3>";
+    while (<$fh>) {
+      last if /<div class='post-footer'>/;
+      #    s/$match/'"'.sprintf("%2d",$i++).$jpgh{$1}.'"'/ge;
+      s/$match/'"'.$inhjpgh{$1}.'"'/ge;
+      print $fho $_;
+    }
+    print $fho $tail;
+    close $fho;
+    close $fh;
+  }
+  chdir("../..");
+}
+
+sub genepub {
+  my $title = shift;
+
+  chdir("$title/epub");
+#  my @htmls= <*-2.html>;
+  # Create EPUB object
+  my $epub = EBook::EPUB->new;
+  # Set metadata: title/author/language/id
+  $epub->add_title($title);
+  $epub->add_author('Green Horn');
+  $epub->add_language('en');
+  $epub->add_identifier('1440465908', 'ISBN');
+
+  # Add package content: stylesheet, font, xhtml and cover
+  #    $epub->copy_stylesheet('/path/to/style.css', 'style.css');
+  $epub->copy_file($_, $_, 'image/jpeg') for (<*.jpg>,<*.jpg.[0-9]>);
+  $epub->copy_file($_, $_, 'image/bmp') for (<*.bmp>,<*.bmp.[0-9]>);
+  $epub->copy_file($_, $_, 'image/png') for (<*.png>,<*.png.[0-9]>);
+  $epub->copy_file($_, $_, 'image/gif') for (<*.gif>,<*.gif.[0-9]>);
+
+  my $po = 1;
+
+  my @htmls = sort {$titleh{$a} cmp $titleh{$b}} keys %titleh;
+  for my $html (@htmls) {
+      my $chapter_id = $epub->copy_xhtml($html, $html);
+      my $navp = $epub->add_navpoint(
+          label       => $titleh{$html},
+          id          => $chapter_id,
+          content     => $html,
+          play_order  => $po++,
+          );
+  }
+
+  # Generate resulting ebook
+  $epub->pack_zip('a.epub');
+
+  chdir("../..");
+}
+
+#system("perl ../03-gen-toc.pl cover.htm > toc.htm");
+#system("perl ../04-gen-epub.pl $title");
+
