6e2faf6045c024625d8945af9d9ebb6c9313d973
liyuray <liyuray@gmail.com>
2013-06-04 15:15:49 +0800

    remove unused lines

--- blogger2epub.pl
+++ blogger2epub.pl
@@ -1,7 +1,6 @@
 use strict;
 use warnings;
 use File::Copy;
-#use File::Slurp;
 use EBook::EPUB;
 use File::Path qw(make_path);
 use Encode qw(decode encode);
@@ -40,20 +39,6 @@ for my $title (@titles) {
   genepub($title);
 }
 
-sub get_head {
-  my $title = shift;
-  my $head = << "AAAA";
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xmlns:xml="http://www.w3.org/XML/1998/namespace" xml:lang="zh" lang="zh">
-<head>
-	<title>$title</title>
-</head>
-<body>
-AAAA
-  return $head;
-}
-
 sub preprocess {
   my $title = shift;
 
@@ -88,7 +73,6 @@ sub preprocess {
 #    $inhjpg =~ s/\'/&#39;/g;
     $njpg =~ tr/a-zA-Z0-9.//cd;
     $njpg =~ s/\.(jpg|bmp|png|gif)\.(\d)/__$2\.$1/i;
-    #print $njpg,$/;
     if ($jpg ne $njpg) {
         $jpgh{$jpg} = sprintf("%04d",$i++)."_".$njpg;
         $inhjpgh{$inhjpg} = $jpgh{$jpg};
@@ -97,16 +81,7 @@ sub preprocess {
         $inhjpgh{$inhjpg} = $jpgh{$jpg};
     }
   }
-#  die;
-#  $jpgh{"Global+Bond+Fund+Holding%25E7%259B%25A7.jpg"} = "93GlobalBondFundHoldingE79BA7.jpg";
-#  $jpgh{"Global+Bond+Fund+Holding%25E7%25BE%258E%25E5%259C%258B.jpg"} = "94GlobalBondFundHoldingE7BE8EE59C8B.jpg";
-#  $jpgh{"%25E7%25BE%258E%25E5%259C%258Bvs%25E7%259B%25A7%25E6%25A3%25AE%25E5%25A0%25A1%25E7%25B8%25BE%25E6%2595%2588.jpg"} = "53E7BE8EE59C8BvsE79BA7E6A3AEE5A0A1E7B8BEE69588.jpg";
-
-#  my $pattern = '(' . join('|', map {quotemeta} grep {$inhjpgh{$_} ne $_} keys %inhjpgh). ')';
-#  my $match   = qr/"$pattern"/;
-#  print $match;
   chdir("$title");
-  #my $i;
   copy("raw/".$b5jpgh{$_}, "epub/".$jpgh{$_}) for @ucjpgs;
   chdir("..");
 
@@ -115,21 +90,11 @@ sub preprocess {
 
   for my $file (@files) {
     my $fnoo = $file;
-#    $fnoo =~ s/\.html$/-2.html/;
-    my $fno = "../epub/$fnoo";
-#    $fno =~ s/\/raw\//\/epub\//;
-#    open my $fh, "<:utf8", $file or die "cannot open < $!";
     my $html = read_file($file, binmode => ':utf8');
-    open my $fho, ">:utf8", $fno or die "cannot open > $!";
+    open my $fho, ">:utf8", "../epub/$fnoo" or die "cannot open > $!";
 
     my $dom = Mojo::DOM->new($html);
     $titleh{$fnoo} = $dom->at('title')->text;
-#    $dom->at('html')->attrs('xml:lang'=>'zh','lang'=>'zh');
-#    $dom->at('body')->attrs(oncontextmenu=>'',ondragstart=>'',onselectstart => '');
-#    for my $img (@imgs) {
-#        my $key = $img->attrs('src');
-#        $img->attrs( src => $inhjpgh{$key} ) if exists $inhjpgh{$key};
-#    }
     my $odom = Mojo::DOM->new;
     $odom->tree(['root',
                  ['tag', 'html', {'xmlns' => 'http://www.w3.org/1999/xhtml'}, {},
@@ -152,22 +117,9 @@ sub preprocess {
                                    $_[0]->attrs( src => $inhjpgh{$key} ) if exists $inhjpgh{$key};
                                  });
     my $output = $odom->root->to_xml;
-    #    while (<$fh>) {
-#      $titleh{$fnoo} = $1 if /<title>(.*)<\/title>/;
-#      last if /<h3 class='post-title entry-title'>/;
-#    }
-#    print $fho get_head($titleh{$fnoo});
-#    print $fho "<h3>";
-#    for (@lines) {
-      #      last if /<div class='post-footer'>/;
-      #    s/$match/'"'.sprintf("%2d",$i++).$jpgh{$1}.'"'/ge;
-#    $output =~ s/$match/'"'.$inhjpgh{$1}.'"'/ge if 0;
     say $fho q(<?xml version="1.0" encoding="utf-8"?>);
     say $fho $output;
-    #    }
-#    print $fho $tail;
     close $fho;
-#    close $fh;
   }
   chdir("../..");
 }
@@ -176,7 +128,6 @@ sub genepub {
   my $title = shift;
 
   chdir("$title/epub");
-#  my @htmls= <*-2.html>;
   # Create EPUB object
   my $epub = EBook::EPUB->new;
   # Set metadata: title/author/language/id
@@ -210,7 +161,3 @@ sub genepub {
 
   chdir("../..");
 }
-
-#system("perl ../03-gen-toc.pl cover.htm > toc.htm");
-#system("perl ../04-gen-epub.pl $title");
-
